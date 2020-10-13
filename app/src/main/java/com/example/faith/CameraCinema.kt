e50068072172b3bbbb727ab2b6157e6674d19f4c
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2020-10-13 13:39:41 +0200

    implementatie video nemen
    implementatie video nemen

--- app/src/main/java/com/example/faith/CameraCinema.kt
+++ app/src/main/java/com/example/faith/CameraCinema.kt
@@ -3,8 +3,11 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
+import android.opengl.Visibility
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -15,6 +18,8 @@ class CameraCinema : AppCompatActivity() {
     private final val REQUEST_PERMISSION = 100
     private final val REQUEST_IMAGE_CAPTURE = 1
     private final val REQUEST_VIDEO_CAPTURE = 1
+    private final val VIDEO_MADE = 2
+    private final val PICTURE_TAKEN = 1
     public var last_code = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +47,7 @@ class CameraCinema : AppCompatActivity() {
         }
     }
     private fun openCameraPicture() {
+        hidePreviews()
         last_code=1
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
             intent.resolveActivity(packageManager)?.also {
@@ -50,6 +56,7 @@ class CameraCinema : AppCompatActivity() {
         }
     }
     private fun openCameraVideo(){
+        hidePreviews()
         last_code=2
         Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { intent ->
             intent.resolveActivity(packageManager)?.also {
@@ -58,8 +65,15 @@ class CameraCinema : AppCompatActivity() {
         }
 
     }
+    private fun hidePreviews(){
+        ivImage.visibility = View.GONE
+        videoView.visibility=View.GONE
+
+    }
     private fun openGallery() {
+        hidePreviews()
         Intent(Intent.ACTION_PICK).also { intent ->
+
             intent.type = "image/*"
             intent.resolveActivity(packageManager)?.also {
                 startActivityForResult(intent, REQUEST_PERMISSION)
@@ -69,18 +83,25 @@ class CameraCinema : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK) {
-            if (requestCode == REQUEST_IMAGE_CAPTURE &&last_code==1) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE &&last_code==PICTURE_TAKEN) {
                 val bitmap = data?.extras?.get("data") as Bitmap
                 ivImage.setImageBitmap(bitmap)
+                ivImage.visibility = View.VISIBLE
             }
             else if (requestCode == REQUEST_PERMISSION && resultCode== RESULT_OK) {
-                val uri = data?.getData()
+                val uri = data?.data
                 ivImage.setImageURI(uri)
+                ivImage.visibility = View.VISIBLE
                 //todo de foto niet alleen weergeven maar ook opslaan in db
             }
-            else if (requestCode == REQUEST_VIDEO_CAPTURE &&last_code==2) {
-                val bitmap = data?.extras?.get("data") as Bitmap
-                ivImage.setImageBitmap(bitmap)
+            else if (requestCode == REQUEST_VIDEO_CAPTURE &&last_code==VIDEO_MADE) {
+
+                var videoUri = data?.data
+                videoView.visibility=View.VISIBLE
+               videoView.setVideoURI(videoUri)
+
+                videoView.start()
+
             }
         }
     }
