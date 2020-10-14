a50d3eccaf4a771656a78f9701681f77346be16b
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2020-10-14 11:29:02 +0200

    foto's worden nu in hoge kwaliteit opgeslagen het apparaat
    foto's worden nu in hoge kwaliteit opgeslagen het apparaat

--- app/src/main/java/com/example/faith/CameraCinema.kt
+++ app/src/main/java/com/example/faith/CameraCinema.kt
@@ -1,8 +1,11 @@
 package com.example.faith
+
 import android.Manifest
 import android.content.Intent
+import android.content.Intent.ACTION_MEDIA_SCANNER_SCAN_FILE
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
@@ -10,12 +13,13 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.cinema_camera.*
 import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
-
 
 class CameraCinema : AppCompatActivity() {
     private final val REQUEST_PERMISSION = 100
@@ -23,27 +27,70 @@ class CameraCinema : AppCompatActivity() {
     private final val REQUEST_VIDEO_CAPTURE = 1
     private final val VIDEO_MADE = 2
     private final val PICTURE_TAKEN = 1
-    public var last_code = 0
+    lateinit var currentPhotoPath: String
+    var last_code = 0
+    val REQUEST_TAKE_PHOTO = 1
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+        super.onCreate(savedInstanceState);
+
+        checkPermission()
+
+
         setContentView(R.layout.cinema_camera)
+
         btCapturePhoto.setOnClickListener {
             openCameraPicture()
         }
         btOpenGallery.setOnClickListener {
             openGallery()
         }
-        btCaptureVideo.setOnClickListener{
+        btCaptureVideo.setOnClickListener {
             openCameraVideo()
         }
     }
+
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1
+            );
+        }
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1
+            );
+        }
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 1);
+        }
+    }
+
     override fun onResume() {
         super.onResume()
         checkCameraPermission()
     }
+
     private fun checkCameraPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-            != PackageManager.PERMISSION_GRANTED) {
+            != PackageManager.PERMISSION_GRANTED
+        ) {
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(Manifest.permission.CAMERA),
@@ -51,18 +98,37 @@ class CameraCinema : AppCompatActivity() {
             )
         }
     }
+
     private fun openCameraPicture() {
-        hidePreviews()
-        last_code=1
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
-            intent.resolveActivity(packageManager)?.also {
-                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+
+
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.example.faith.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                }
             }
         }
     }
-    private fun openCameraVideo(){
+
+    private fun openCameraVideo() {
         hidePreviews()
-        last_code=2
+        last_code = 2
         Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { intent ->
             intent.resolveActivity(packageManager)?.also {
                 startActivityForResult(intent, REQUEST_VIDEO_CAPTURE)
@@ -70,11 +136,13 @@ class CameraCinema : AppCompatActivity() {
         }
 
     }
-    private fun hidePreviews(){
+
+    private fun hidePreviews() {
         ivImage.visibility = View.GONE
-        videoView.visibility=View.GONE
+        videoView.visibility = View.GONE
 
     }
+
     private fun openGallery() {
         hidePreviews()
         Intent(Intent.ACTION_PICK).also { intent ->
@@ -86,33 +154,49 @@ class CameraCinema : AppCompatActivity() {
         }
     }
 
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        ).apply {
+            currentPhotoPath = absolutePath
+        }
+    }
 
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, intent1: Intent?) {
-        super.onActivityResult(requestCode, resultCode, intent1)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, theIntent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, theIntent)
         if (resultCode == RESULT_OK) {
-            if (requestCode == REQUEST_IMAGE_CAPTURE &&last_code==PICTURE_TAKEN) {
-                val bitmap = intent1?.extras?.get("data") as Bitmap
+            if (requestCode == REQUEST_IMAGE_CAPTURE && last_code == PICTURE_TAKEN) {
+                val bitmap = theIntent?.extras?.get("data") as Bitmap
                 ivImage.setImageBitmap(bitmap)
                 ivImage.visibility = View.VISIBLE
+                galleryAddPic()
 
-            }
-            else if (requestCode == REQUEST_PERMISSION && resultCode== RESULT_OK) {
-                val uri = intent1?.data
+            } else if (requestCode == REQUEST_PERMISSION && resultCode == RESULT_OK) {
+                val uri = theIntent?.data
                 ivImage.setImageURI(uri)
                 ivImage.visibility = View.VISIBLE
                 //todo de foto niet alleen weergeven maar ook opslaan in db
-            }
-            else if (requestCode == REQUEST_VIDEO_CAPTURE &&last_code==VIDEO_MADE) {
+            } else if (requestCode == REQUEST_VIDEO_CAPTURE && last_code == VIDEO_MADE) {
 
-                var videoUri = intent1?.data
-                videoView.visibility=View.VISIBLE
-               videoView.setVideoURI(videoUri)
-
+                var videoUri = theIntent?.data
+                videoView.visibility = View.VISIBLE
+                videoView.setVideoURI(videoUri)
                 videoView.start()
 
             }
+        }
+    }
+    private fun galleryAddPic() {
+        Intent(ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+            val f = File(currentPhotoPath)
+            mediaScanIntent.data = Uri.fromFile(f)
+            sendBroadcast(mediaScanIntent)
         }
     }
 
