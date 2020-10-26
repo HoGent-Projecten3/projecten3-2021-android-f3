3e5e6a29ee0bc46b7c5e664af7603d4e16f62e7e
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2020-10-26 11:34:20 +0100

    Add  start detail medium
    Add  start detail medium

---
+++ app/src/main/java/com/example/faith/MediumDetailFragment.kt
@@ -0,0 +1,71 @@
+package com.example.faith
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.example.faith.MediumDetailFragment.Callback
+import com.example.faith.data.Medium
+import com.example.faith.databinding.FragmentMediumDetailBinding
+import com.example.faith.viewmodels.MediumDetailViewModel
+import com.example.faith.viewmodels.MediumDetailViewModel.Companion.provideFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MediumDetailFragment : Fragment() {
+
+    private val args: MediumDetailFragmentArgs by navArgs()
+
+
+    @Inject
+    lateinit var mediumDetailViewModelFactory: MediumDetailViewModel.AssistedFactory
+
+
+
+    private val mediumDetailViewModel: MediumDetailViewModel by viewModels {
+        provideFactory(
+            mediumDetailViewModelFactory,
+            args.mediumId
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentMediumDetailBinding>(
+            inflater,
+            R.layout.fragment_medium_detail,
+            container,
+            false
+        ).apply {
+            viewModel = mediumDetailViewModel
+            lifecycleOwner = viewLifecycleOwner
+            callback = Callback { medium ->
+                medium?.let {
+
+
+                }
+            }
+
+
+
+            }
+
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+
+
+    fun interface Callback {
+        fun add(medium: Medium?)
+    }
+    }
---
+++ app/src/main/java/com/example/faith/viewmodels/MediumDetailViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.faith.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.faith.data.MediumRepository
+import com.squareup.inject.assisted.Assisted
+import com.squareup.inject.assisted.AssistedInject
+
+class MediumDetailViewModel @AssistedInject constructor(
+    private val mediumRepository: MediumRepository,
+    @Assisted private val mediumId: Int
+) :ViewModel() {
+
+
+    val medium = mediumRepository.getMedium(mediumId)
+
+
+
+    @AssistedInject.Factory
+    interface AssistedFactory {
+        fun create(mediumId: Int): MediumDetailViewModel
+    }
+
+    companion object {
+        fun provideFactory(
+            assistedFactory: AssistedFactory,
+            mediumId: Int
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return assistedFactory.create(mediumId) as T
+            }
+        }
+    }
+}
