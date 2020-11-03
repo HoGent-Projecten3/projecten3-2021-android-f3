aa4b101b595759eb0735faa3bea075f01eb56ec6
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2020-11-03 22:17:55 +0100

    Add opmaak

--- app/src/main/java/com/example/faith/viewmodels/DagboekDetailViewModel.kt
+++ app/src/main/java/com/example/faith/viewmodels/DagboekDetailViewModel.kt
@@ -1,19 +1,33 @@
 package com.example.faith.viewmodels
 
+import android.os.Message
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.example.faith.data.MediumRepository
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
+import kotlinx.coroutines.launch
+import retrofit2.Call
+
 /**
  * @author Remi Mestdagh
  */
 class DagboekDetailViewModel @AssistedInject constructor(
-    mediumRepository: MediumRepository,
+    private val mediumRepository: MediumRepository,
     @Assisted private val mediumId: Int
 ) : ViewModel() {
 
     var dagboek = mediumRepository.getMedium(mediumId)
+
+    fun deleteMediumRoom(mediumId: Int){
+        viewModelScope.launch {
+            mediumRepository.deleteMediumRoom(mediumId)
+        }
+    }
+    fun removeMediumApi(): Call<Message> {
+        return mediumRepository.removeMedium(dagboek.value!!.mediumId)
+    }
 
     @AssistedInject.Factory
     interface AssistedFactory {
