23c6ee9c4fccf437c463d31296e0c5ad3f6a8f57
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2020-11-16 12:04:18 +0100

    Add werkende beschrijving media

--- app/src/main/java/com/example/faith/api/SignalRService.kt
+++ app/src/main/java/com/example/faith/api/SignalRService.kt
@@ -22,7 +22,7 @@ class SignalRService {
 
     fun start(email: String?, chatFragment: ChatFragment) {
         this.chatFragment = chatFragment
-        hubConnection = HubConnectionBuilder.create("https://f3backend-dev-as.azurewebsites.net/connectionHub")
+        hubConnection = HubConnectionBuilder.create("http://192.168.1.37:45455/connectionHub")
             .withAccessTokenProvider(
                 Single.defer {
                     Single.just(
