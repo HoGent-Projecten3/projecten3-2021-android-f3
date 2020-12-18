dbb2bf74047709fd0be129ca2c22975d1c733646
Remi Mestdagh <remi.mestdagh@student.hogent.be>
2020-12-18 17:59:10 +0100

    Add working chat test
    kaas

---
+++ app/src/androidTest/java/com/example/faith/ChatTest.kt
@@ -0,0 +1,84 @@
+package com.example.faith
+
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.rule.ActivityTestRule
+import com.example.faith.api.ApiService
+import com.example.faith.api.MyServiceInterceptor
+import com.example.faith.api.SignalRService
+import com.example.faith.data.ApiBerichtSearchResponse
+import com.example.faith.data.AppDatabase
+import com.example.faith.data.Bericht
+import com.example.faith.data.BerichtDao
+import com.example.faith.data.BerichtRepository
+import com.example.faith.data.GebruikerRepository
+import com.example.faith.utilities.RecyclerViewItemCountAssertion
+import com.example.faith.viewmodels.ChatViewModel
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import java.time.LocalDateTime
+import java.util.*
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.spy
+import com.nhaarman.mockitokotlin2.whenever
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+import org.junit.rules.RuleChain
+import java.util.regex.Pattern.matches
+
+@HiltAndroidTest
+class ChatTest {
+
+    var datum = LocalDateTime.now().toString()
+    var bericht1 = Bericht(0, "", "", "", "", "", Date())
+    var berichten = listOf(bericht1)
+    var berichtResponse = ApiBerichtSearchResponse(berichten, datum, 20)
+    val activityTestRule = ActivityTestRule(MainActivity::class.java)
+    private val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    val rule = RuleChain
+        .outerRule(hiltRule)
+        .around(activityTestRule)
+
+    private val apiService: ApiService = mock()
+    private val signalRService: SignalRService = SignalRService()
+    private val appDB: AppDatabase = mock()
+    var berichtDao: BerichtDao = mock()
+    var interceptor: MyServiceInterceptor = MyServiceInterceptor(signalRService)
+    var gebruikerRepository: GebruikerRepository = GebruikerRepository(apiService, interceptor)
+    var berichtRepo: BerichtRepository = BerichtRepository(apiService, berichtDao)
+
+    var chatViewModel: ChatViewModel = ChatViewModel(gebruikerRepository, berichtRepo)
+
+    @Before
+    fun setup() {
+        this.chatViewModel = ChatViewModel(gebruikerRepository, berichtRepo)
+        activityTestRule.activity.apply {
+            runOnUiThread {
+                findNavController(R.id.myNavHostFragment).navigate(R.id.chatFragment)
+            }
+        }
+    }
+
+    @Test
+    fun not() {
+        whenever(this.berichtRepo.getBerichten2(datum, 20)).thenAnswer {
+            return@thenAnswer berichtResponse
+        }
+        whenever(this.berichtRepo.verstuurBericht("", "", "", "", "")).thenAnswer {
+            return@thenAnswer bericht1
+        }
+        onView(withId(R.id.txfEditBericht)).perform(ViewActions.typeText("Hallo ik ben Bejamin"))
+        onView(withId(R.id.btSendMessage)).perform(click())
+
+        onView(withId(R.id.messages_list))
+            .check(RecyclerViewItemCountAssertion(1));
+    }
+}
--- app/src/androidTest/java/com/example/faith/utilities/TestUtils.kt
+++ app/src/androidTest/java/com/example/faith/utilities/TestUtils.kt
@@ -1,6 +1,12 @@
 package com.example.faith.utilities
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.NoMatchingViewException
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import com.example.faith.data.Medium
+import org.hamcrest.CoreMatchers.`is`
 import java.util.Date
 
 val testMedia = arrayListOf(
@@ -11,3 +17,14 @@ val testMedia = arrayListOf(
 )
 
 val testMedium = testMedia[0]
+
+class RecyclerViewItemCountAssertion(private val expectedCount: Int) : ViewAssertion {
+    override fun check(view: View, noViewFoundException: NoMatchingViewException?) {
+        if (noViewFoundException != null) {
+            throw noViewFoundException
+        }
+        val recyclerView = view as RecyclerView
+        val adapter = recyclerView.adapter
+        assertThat(adapter!!.itemCount, `is`(expectedCount))
+    }
+}
